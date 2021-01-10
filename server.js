d9edb8b58982aac0f8847e47a6f159e1b9427c2f
prlouis <pierrereama.louis@gmail.com>
2021-01-09 23:42:37 -0500

    added slideshow version

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const app = express();
 
 app.use(express.static(`${__dirname}/public`));
 
-// Send default page in french
+// Send default page in english
 app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/index-en.html`);
+});
+
+// Send page in french
+app.get('/fr', (req, res) => {
   res.sendFile(`${__dirname}/public/index-fr.html`);
 });
 
-// Send page in english
-app.get('/en', (req, res) => {
-  res.sendFile(`${__dirname}/public/index-en.html`);
+app.get('/slideshow', (req, res) => {
+  res.sendFile(`${__dirname}/public/slideshow.html`);
 });
 
 app.use('/', router);
