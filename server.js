b597be296f14212eb489129e8892b94b44814bf7
prlouis <pierrereama.louis@gmail.com>
2021-01-11 00:18:10 -0500

    added different example options

--- server.js
+++ server.js
@@ -7,21 +7,35 @@ const port = 3000;
 app.use(express.static(`${__dirname}/public`));
 
 // Send default page in english
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/public/index-en.html`);
+app.get('/example1', (req, res) => {
+  res.sendFile(`${__dirname}/public/white-collar.html`);
 });
 
 // Send page in french
-app.get('/fr', (req, res) => {
-  res.sendFile(`${__dirname}/public/index-fr.html`);
+app.get('/example1-fr', (req, res) => {
+  res.sendFile(`${__dirname}/public/white-collar-fr.html`);
+});
+
+// Send default page in english
+app.get('/example2', (req, res) => {
+  res.sendFile(`${__dirname}/public/on-chair.html`);
+});
+
+// Send page in french
+app.get('/example2-fr', (req, res) => {
+  res.sendFile(`${__dirname}/public/on-chair-fr.html`);
 });
 
 app.get('/slideshow', (req, res) => {
   res.sendFile(`${__dirname}/public/slideshow.html`);
 });
 
+app.get('/slideshow-fr', (req, res) => {
+  res.sendFile(`${__dirname}/public/slideshow-fr.html`);
+});
+
 app.use('/', router);
 
 app.listen(port, hostname, () => {
-	console.log(`Server running at http:/\/${hostname}:${port}/`); 
+  console.log(`Server running at http:/\/${hostname}:${port}/`);
 });
